9ceaf8b4f3c4d117c7d13d0b21b8185513125e3a
Tu Dinh <ngoc-tu.dinh@vates.tech>
2025-07-10 10:54:49 +0200

    Opt into forced unplug
    Condition the opt-in to the FORCE_UNPLUG build variable.
    
    Signed-off-by: Tu Dinh <ngoc-tu.dinh@vates.tech>

--- scripts/genfiles.ps1
+++ scripts/genfiles.ps1
@@ -68,6 +68,8 @@ $Replacements = [ordered]@{
 	'MICRO_VERSION' = $Env:MICRO_VERSION;
 	'BUILD_NUMBER' = $Env:BUILD_NUMBER;
 
+	'FORCE_UNPLUG' = $Env:FORCE_UNPLUG;
+
 	# generated values
 	'GIT_REVISION' = $GitRevision;
 
