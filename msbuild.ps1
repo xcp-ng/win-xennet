e9682cb9eda3b6461f26a41dccc7733bf51942e6
Owen Smith <owen.smith@cloud.com>
2025-09-08 08:33:24 +0100

    Add CodeAnalysis switch to build.ps1
    Add option to enable code analysis on builds, so that developers can
    get code analysis feedback quicker than relying on SDV builds.
    
    Signed-off-by: Owen Smith <owen.smith@cloud.com>

--- msbuild.ps1
+++ msbuild.ps1
@@ -8,7 +8,8 @@ param(
 	[string]$Arch,
 	[Parameter(Mandatory = $true)]
 	[string]$Type,
-	[string]$SignMode = "TestSign"
+	[string]$SignMode = "TestSign",
+	[switch]$CodeAnalysis
 )
 
 Function Run-MSBuild {
@@ -154,10 +155,10 @@ $projectlist = @( "xennet" )
 Set-ExecutionPolicy -Scope CurrentUser -Force Bypass
 
 if ($Type -eq "free") {
-	Run-MSBuild $solutionpath "xennet.sln" $configuration["free"] $platform[$Arch]
+	Run-MSBuild $solutionpath "xennet.sln" $configuration["free"] $platform[$Arch] -CodeAnalysis:$CodeAnalysis
 }
 elseif ($Type -eq "checked") {
-	Run-MSBuild $solutionpath "xennet.sln" $configuration["checked"] $platform[$Arch]
+	Run-MSBuild $solutionpath "xennet.sln" $configuration["checked"] $platform[$Arch] -CodeAnalysis:$CodeAnalysis
 }
 elseif ($Type -eq "codeql") {
 	if (-Not (Test-Path -Path $archivepath)) {
